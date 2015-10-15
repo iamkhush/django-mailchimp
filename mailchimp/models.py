339e24c5c9759e76872903696e6f3233c7e05134
Federico Bond <federicobond@gmail.com>
2015-10-15 14:19:38 -0300

    Update module name for GenericForeignKey

--- mailchimp/models.py
+++ mailchimp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import json as simplejson
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
@@ -75,7 +75,7 @@ class Queue(models.Model):
     type_opts = models.TextField()
     content_type = models.ForeignKey(ContentType, null=True, blank=True)
     object_id = models.PositiveIntegerField(null=True, blank=True)
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     extra_info = models.TextField(null=True)
     locked = models.BooleanField(default=False)
     
@@ -195,7 +195,7 @@ class Campaign(models.Model):
     name = models.CharField(max_length=255)
     content_type = models.ForeignKey(ContentType, null=True, blank=True)
     object_id = models.PositiveIntegerField(null=True, blank=True)
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     extra_info = models.TextField(null=True)
     
     objects = CampaignManager()
